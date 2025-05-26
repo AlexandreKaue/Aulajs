1edfcfd4f47c03a6480631cd3e4e15c035499141
AlexandreKaue <4lexandrekaue@gmail.com>
2025-05-26 16:28:29 -0300

    Aula 3 e 4

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 document.getElementById("texto") .innerHTML="Meu primeiro texto <b>Js<b>!";
+console.log("Oi isso aqui é um console.log");
+let pessoa = "Dimitri"
+var  a = 1
+var  b = 2
+var c = a + b ;
+console.log(c);
+
+
